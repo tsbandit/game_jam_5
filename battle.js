43d32d27d815fefa1606d5e9e2eb3ad58bf1a692
Joe Allington <joecanmakeit@gmail.com>
2015-09-19 16:51:35 -0700

    Enemies now occasionally attack.

--- battle.js
+++ battle.js
@@ -5,56 +5,85 @@ window.modules[moduleName] = (function () {
 	// Battle screen
 	var module = {};
     
-    var Combatant = function(name, hp, str, speed) {
+    var Combatant = function(name, hp, dmg, speed) {
         this.name = name;
         this.hp = hp;
-        this.str = str;
+        this.dmg = dmg;
         this.speed = speed;
         this.cd = 0;
     };
+    
+    Combatant.prototype.attack = function(target) {
+        target.hp -= this.dmg;
+    };
+    
+    Combatant.prototype.die = function(isAlly) {
+        this.hp = 0;
+    }
     
     var allies = [];
     var enemies = [];
 	
 	module.initUi = function () {
         
-        allies.push(new Combatant("Alice", 10, 3, 0.8));
-        allies.push(new Combatant("Bob", 8, 3, 0.8));
-        allies.push(new Combatant("Carl", 9, 3, 0.8));
+        allies.push(new Combatant("Alice", 10, 3, 1.4));
+        allies.push(new Combatant("Bob", 8, 3, 1.4));
+        allies.push(new Combatant("Carl", 9, 3, 1.4));
         
-        enemies.push(new Combatant("Foo", 3, 2, 0.7));
-        enemies.push(new Combatant("Bar", 4, 2, 0.6));
-        enemies.push(new Combatant("Baz", 5, 2, 0.63));
+        enemies.push(new Combatant("Foo", 3, 2, 8.5));
+        enemies.push(new Combatant("Bar", 4, 2, 6.5));
+        enemies.push(new Combatant("Baz", 5, 2, 7.2));
         
         var drawAllies = function(ctx) {
             var Game = window.modules.game;
-            ctx.font = "bold 18pt sans-serif";
             ctx.textAlign = "left";
-            ctx.fillStyle = "#0f0";
             for (var i=0; i<allies.length; i++) {
-                var e = allies[i];
-                ctx.fillText(e.name + " " + e.hp, 20, 22*(i+1));
+                var a = allies[i];
+                ctx.font = "bold 18pt sans-serif";
+                ctx.fillStyle = "#0f0";
+                if (a.hp <= 0) ctx.fillStyle = "#888";
+                ctx.fillText(a.name + " " + a.hp, 20, 22*(i+1));
             }
-        }
+        };
         
         var drawEnemies = function(ctx) {
             var Game = window.modules.game;
-            ctx.font = "bold 18pt sans-serif";
             ctx.textAlign = "right";
-            ctx.fillStyle = "#f00";
             for (var i=0; i<enemies.length; i++) {
                 var e = enemies[i];
                 var txt = e.name + " " + e.hp;
+                ctx.font = "bold 18pt sans-serif";
+                ctx.fillStyle = "#f00";
+                if (e.hp <= 0) ctx.fillStyle = "#888";
                 ctx.fillText(txt, Game.WIDTH-20, 22*(i+1));
-                ctx.fillText(e.cd.toFixed(2), Game.WIDTH-120, 22*(i+1));
+                //ctx.font = "bold 14pt sans-serif";
+                //ctx.fillStyle = "#444";
+                //ctx.fillText(e.cd.toFixed(2), Game.WIDTH-ctx.measureText(txt).width-44, 22*(i+1));
             }
-        }
+        };
         
         var tickEnemies = function(elapsed) {
             for (var i=0; i<enemies.length; i++) {
-                enemies[i].cd += 0.01*elapsed;
+                var e = enemies[i];
+                e.cd += elapsed;
+                if (e.cd >= e.speed*1000) {
+                        if (!alliesDead()) {
+                        var t = allies[Math.floor(Math.random()*allies.length)];
+                        while (t.hp <= 0) t = allies[Math.floor(Math.random()*allies.length)];
+                        e.attack(t);
+                        if (t.hp <= 0) t.die(true);
+                        e.cd -= e.speed*1000;
+                    }
+                }
             }
-        }
+        };
+        
+        var alliesDead = function() {
+            for (var i=0; i<allies.length; i++) {
+                if (allies[i].hp > 0) return false;
+            }
+            return true;
+        };
         
 		var ui = {
 			draw: function (ctx) {
@@ -65,7 +94,7 @@ window.modules[moduleName] = (function () {
 				tickEnemies(elapsed);
 			},
             mouse_moved: () => {
-                enemies[0].hp += 3;
+                //enemies[0].hp += 3;
             },
 		};
 		return ui;
