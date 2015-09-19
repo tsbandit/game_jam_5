75e887ea4509e633b8c2858dc7da74fd541da757
Tommy <Tommy>
2015-09-19 15:54:32 -0700

    added map_screen.js

--- map_screen.js
+++ map_screen.js
@@ -1,25 +1,24 @@
-var moduleName = "title";
+var moduleName = "map_screen";
 
 window.modules = window.modules || {};
 window.modules[moduleName] = (function () {
 	// Title screen
-	var exports = {};
+	const exports = {};
 	
 	exports.initUi = function () {
 		var Game = window.modules.game;
 		var ui = {
 			draw: function (ctx) {
-				
 				ctx.font = "bold 24pt sans-serif";
 				ctx.fillStyle = "#000";
 				ctx.textAlign = "center";
-				ctx.fillText('TITLE GOES HERE', Game.WIDTH/2, Game.HEIGHT/5);
+
+				ctx.fillText('foo', Game.WIDTH/4, Game.HEIGHT/5);
 			},
 			tick: function (elapsed) {
 				
 			},
 			mouse_clicked: function(ev) {
-				Game.ui = modules.map_screen.initUi();
 			},
 		}
 		return ui;
