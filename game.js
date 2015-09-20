a533f47fd6508f7a71461689806bf01c571d5b80
Eli Marable <edm1234@sbcglobal.net>
2015-09-19 19:10:08 -0700

    Moved the rest of the modules out of main. Main is tiny now.

--- game.js
+++ game.js
@@ -8,11 +8,13 @@ const game = modules.define('game')
 	
 	let ui = {};
 	
-	// Input
+	// Game
 	var exports = {
 		WIDTH,
 		HEIGHT,
 		ui,
+		
+		canvas: null,
 		
 		delimit(new_ui, gen_func) {
 			defs.util.run_async(function*(resume) {
@@ -24,6 +26,36 @@ const game = modules.define('game')
 					ui = old_ui;
 				}
 			});
+		},
+		
+		initCanvas() {
+			// Create canvas
+			const canvas = document.createElement('canvas');
+			canvas.setAttribute('width',  game.WIDTH);
+			canvas.setAttribute('height', game.HEIGHT);
+			document.body.appendChild(canvas);
+			game.canvas = canvas;
+			
+			// Install animation-frame handler
+			const ctx = canvas.getContext('2d');
+			ctx.fillStyle = 'red';
+
+			let prev_timestamp = null;
+			const anim = function(timestamp) {
+				if(prev_timestamp === null)
+					prev_timestamp = timestamp - 16;
+
+				(game.ui.tick || (() => {})) (timestamp - prev_timestamp);
+
+				ctx.clearRect(0, 0, game.WIDTH, game.HEIGHT);
+				(game.ui.draw || (() => {})) (ctx);
+
+				prev_timestamp = timestamp;
+				requestAnimationFrame(anim);
+			};
+			requestAnimationFrame(anim);
+	
+			return canvas;
 		}
 	};
 	
