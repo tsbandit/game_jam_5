a2024b9410ef655a17abda6b8d1ece46fd895ead
Tommy <Tommy>
2015-09-22 20:04:02 -0700

    fixed player sprites on the party menu screen

--- party_screen.js
+++ party_screen.js
@@ -15,7 +15,7 @@ const party_screen = modules.define('party_screen')
 		const statLineHeight = 20;
 		
 		function drawPartyMember(ctx, m, x, y) {
-			image.drawImage(ctx, 'char/hero.png', x + 10, y + 10);
+			image.drawImage(ctx, m.img, x + 10, y + 10);
 			
 			ctx.font = "bold 18pt sans-serif";
 			ctx.fillStyle = "#060";
