ab510fe022ba2229fdc3516d3b1ae28f4408ffdf
Tommy <Tommy>
2015-09-24 18:39:31 -0700

    started reimplementing the party_screen to be an equipment management screen

--- party_screen.js
+++ party_screen.js
@@ -4,48 +4,68 @@ const party_screen = modules.define('party_screen')
 .import('game')
 .import('image')
 .import('battle')
+.import('util')
+.import('input')
 .export(function (defs) {
-    
+	const {image, game, battle, util, input} = defs;
+
+	const over = function(x, y, w, h) {
+		const {mx, my} = input;
+		return  mx >= x  &&  mx < x+w  &&  my >= y  &&  my < y+h;
+	};
+	const draw_button = function(ctx, {x, y, w, h, color, text}) {
+		if(over(x, y, w, h))
+			ctx.fillStyle = color.bright;
+		else
+			ctx.fillStyle = color.dark;
+		ctx.fillRect(x, y, w, h);
+		ctx.font = 'bold 19px sans-serif';
+		ctx.fillStyle = 'white';
+		ctx.textAlign = 'left';
+		ctx.fillText(text, x+.2*h, y+.8*h);
+	};
+	const over_button = function({x, y, w, h}) {
+		return over(x, y, w, h);
+	};
+
     const exports = {};
-	const {image, game, battle} = defs;
-	exports.initUi = function (prevUi) {
-		
-		const statOffset = 260;
-		const statValueOffset = 100;
-		const statLineHeight = 20;
-		
-		function drawPartyMember(ctx, m, x, y) {
-			image.drawImage(ctx, m.pictureName, x + 10, y + 10);
-			
-			ctx.font = "bold 18pt sans-serif";
-			ctx.fillStyle = "#060";
-			ctx.textAlign = "left";
-			ctx.fillText(m.name, x, y);
-			
-			ctx.font = "bold 12pt sans-serif";
-			ctx.fillStyle = "#d00";
-			ctx.fillText('HP', x + statOffset, y);
-			ctx.fillText('POWER', x + statOffset, y + statLineHeight);
-			ctx.fillText('SPEED', x + statOffset, y + statLineHeight * 2);
-			
-			ctx.fillStyle = "#000";
-			ctx.fillText(m.hp + ' / ' + m.maxhp, x + statOffset + statValueOffset, y);
-			ctx.fillText(m.dmg, x + statOffset + statValueOffset, y + statLineHeight);
-			ctx.fillText(m.speed.toFixed(1), x + statOffset + statValueOffset, y + statLineHeight * 2);
-		}
-			
+	exports.initUi = function(prev_ui) {
+		const exit_button = {
+			x: 20,
+			y: game.HEIGHT - 20 - 24,
+			w: 200,
+			h: 24,
+			color: {
+				bright: 'blue',
+				dark:   '#44f',
+			},
+			text: 'Return to game',
+		};
+
 		const ui = {
 			draw: function (ctx) {
-				for (let i = 0; i < battle.allies.length; ++i) {
-					drawPartyMember(ctx, battle.allies[i], 40, 40 + i * 100);
+				util.assert(battle.allies.length === 4);
+
+				draw_button(ctx, exit_button);
+
+				ctx.font = '19px sans-serif';
+				ctx.fillStyle = 'black';
+				ctx.textAlign = 'left';
+
+				ctx.fillText('EQUIPMENT', game.WIDTH/2, 50);
+				for(let i=0; i<battle.allies.length; ++i) {
+					const a = battle.allies[i];
+
+					ctx.fillText(a.name, 50, 50+50*i);
 				}
 			},
 			tick: function (elapsed) {
 				
 			},
 			mouse_clicked: function(ev) {
-				game.ui = prevUi;
-			}
+				if(over_button(exit_button))
+					return game.ui = prev_ui;
+			},
 		};
 
 		return ui;
